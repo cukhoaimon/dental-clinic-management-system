4e85278216fe649c13c58e41dd9b7420751c30bb
21120262 <92104269+hkhangus@users.noreply.github.com>
2023-11-29 15:10:13 +0700

    apply dialog to them, sua, xoa

--- admin/src/features/panel/Panel.jsx
+++ admin/src/features/panel/Panel.jsx
@@ -1,87 +1,133 @@
-import { Fragment, useEffect, useState } from 'react'
-import { ItemSide } from './ItemSide'
-import { MedicineBoard } from './admin/medicine/MedicineBoard'
-import Dialog from '../common/Dialog'
-import useProcessDialog from '../../hooks/useProcessDialog'
-import { sideDataMock } from './mocks/sideData'
+import { Fragment, useEffect, useState } from "react";
+import { ItemSide } from "./ItemSide";
+import { MedicineBoard } from "./admin/medicine/MedicineBoard";
+import Dialog from "../common/Dialog";
+import useProcessDialog from "../../hooks/useProcessDialog";
+import { sideDataMock } from "./mocks/sideData";
 
 export const Panel = () => {
-    const [selectedTitle, setSelectedTitle] = useState('Thuốc')
-    const [sideData, setSideData] = useState([])
-    const [search, setSearch] = useState('')
+  const [selectedTitle, setSelectedTitle] = useState("Thuốc");
+  const [sideData, setSideData] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const handleSearchChange = (e) => {
-        setSearch(e.target.value)
-    }
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const handleSearchSubmit = () => {
-        alert(search)
-    }
+  const handleSearchSubmit = () => {
+    alert(search);
+  };
 
-    const handleSelect = (title) => {
-        setSelectedTitle(title)
-    }
+  const handleSelect = (title) => {
+    setSelectedTitle(title);
+  };
 
-    // load data
-    useEffect(() => {
-        setSideData(sideDataMock)
-    }, [sideData])
-    
-    // handle dialog
-    const [openDialog, setOpenDialog] = useState(false)
+  // load data
+  useEffect(() => {
+    setSideData(sideDataMock);
+  }, [sideData]);
 
-    const attr = useProcessDialog({
-        id: 'test',
-        title: 'test',
-        triggerValue: openDialog,
-        onClose: () => {
-          setOpenDialog(false)
-        },
-      })
+  // handle dialog
+  const [openDialog, setOpenDialog] = useState(false);
+  const [diaLogName, setDialogName] = useState("");
+
+  const attr = useProcessDialog({
+    id: diaLogName,
+    title: diaLogName,
+    triggerValue: openDialog,
+    onClose: () => {
+      setOpenDialog(false);
+    },
+  });
 
   return (
     <Fragment>
-        <div className='nav h-[60px] bg-sky-100 flex items-center px-4 justify-between'>
-            <button className='h-10 w-24 rounded-lg bg-sky-300 text-sky-600 font-bold'>Home</button>
-            <div className='search-bar w-[500px] flex'>
-                <input onChange={handleSearchChange} className='w-3/5 mr-3 px-3 rounded-full' type="text" name="search" id="search" placeholder='Nhập để tìm kiếm theo mã'/>
-                <button onClick={handleSearchSubmit} className='w-1/5 h-10 rounded-lg bg-sky-300 text-sky-600 font-bold'>Tìm</button>
-            </div>
+      <div className="nav flex h-[60px] items-center justify-between bg-sky-100 px-4">
+        <button className="h-10 w-24 rounded-lg bg-sky-300 font-bold text-sky-600">
+          Home
+        </button>
+        <div className="search-bar flex w-[500px]">
+          <input
+            onChange={handleSearchChange}
+            className="mr-3 w-3/5 rounded-full px-3"
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Nhập để tìm kiếm theo mã"
+          />
+          <button
+            onClick={handleSearchSubmit}
+            className="h-10 w-1/5 rounded-lg bg-sky-300 font-bold text-sky-600"
+          >
+            Tìm
+          </button>
         </div>
-        <div className='main-container flex'>
-            <div className='side w-1/4 pt-10'>
-                <div className='title mx-8 mb-10'>
-                    <p className='w-[240px] shadow-lg h-16 bg-sky-300 text-center leading-[64px] text-3xl text-sky-600 rounded-2xl font-bold'>
-                        {selectedTitle}
-                    </p>
-                </div>
+      </div>
+      <div className="main-container flex">
+        <div className="side w-1/4 pt-10">
+          <div className="title mx-8 mb-10">
+            <p className="h-16 w-[240px] rounded-2xl bg-sky-300 text-center text-3xl font-bold leading-[64px] text-sky-600 shadow-lg">
+              {selectedTitle}
+            </p>
+          </div>
 
-                <div className='list'>
-                    {sideData.map((item, index) => (
-                        <ItemSide onClick={handleSelect} key={index} item={item} even={index % 2 === 0} handleTitle={handleSelect}/>
-                    ))}
-                </div>
-            </div>
-            <div className='main w-3/4 bg-gray-200'>
-                <div className='flex px-8 justify-between items-center main-navbar w-full bg-sky-500 h-14'>
-                    <div className='w-28 mt-6 bg-white h-8 rounded-tl-lg rounded-tr-lg'>
-                        <p className='text-sky-600 text-center w-full leading-8'>Table #1</p>
-                    </div>
-                    <div>
-                        <button className='h-8 rounded-md text-sky-600 leading-8 bg-sky-200 w-20 mr-8' onClick={()=>setOpenDialog(true)}>+ Thêm</button>
-                        <Dialog title={'Test dialog'} attr={attr}>
-                            <h1>the quick brown fox jumps over the lazy dog</h1>
-                        </Dialog>
-                        <button className='h-8 rounded-md text-sky-600 leading-8 bg-sky-200 w-20 mr-8'>- Xoá</button>
-                        <button className='h-8 rounded-md text-sky-600 leading-8 bg-sky-200 w-20'>? Sửa</button>
-                    </div>
-                </div>
-
-                <div className='main-content w-full px-10 py-5'>
-                    <MedicineBoard />
-                </div>
-            </div>
+          <div className="list">
+            {sideData.map((item, index) => (
+              <ItemSide
+                onClick={handleSelect}
+                key={index}
+                item={item}
+                even={index % 2 === 0}
+                handleTitle={handleSelect}
+              />
+            ))}
+          </div>
         </div>
+        <div className="main w-3/4 bg-gray-200">
+          <div className="main-navbar flex h-14 w-full items-center justify-between bg-sky-500 px-8">
+            <div className="mt-6 h-8 w-28 rounded-tl-lg rounded-tr-lg bg-white">
+              <p className="w-full text-center leading-8 text-sky-600">
+                Table #1
+              </p>
+            </div>
+            <div>
+              <button
+                className="mr-8 h-8 w-20 rounded-md bg-sky-200 leading-8 text-sky-600"
+                onClick={() => {
+                  setOpenDialog(true);
+                  setDialogName("Thêm");
+                }}
+              >
+                + Thêm
+              </button>
+
+              <button className="mr-8 h-8 w-20 rounded-md bg-sky-200 leading-8 text-sky-600"
+              onClick={() => {
+                setOpenDialog(true);
+                setDialogName("Xoá");
+              }}>
+                - Xoá
+              </button>
+
+              <button className="h-8 w-20 rounded-md bg-sky-200 leading-8 text-sky-600"
+              onClick={() => {
+                setOpenDialog(true);
+                setDialogName("Sửa");
+              }}>
+                ? Sửa
+              </button>
+
+              <Dialog title={diaLogName} attr={attr}>
+                <h1>the quick brown fox jumps over the lazy dog</h1>
+              </Dialog>
+            </div>
+          </div>
+
+          <div className="main-content w-full px-10 py-5">
+            <MedicineBoard />
+          </div>
+        </div>
+      </div>
     </Fragment>
-  )
-}
+  );
+};
