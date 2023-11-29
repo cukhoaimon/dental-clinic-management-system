cc87b1295e6e649e99f6adf8cd8e78bca7b37c26
21120262 <92104269+hkhangus@users.noreply.github.com>
2023-11-29 15:05:38 +0700

    complete convert dialog css to tailwind

--- admin/src/features/common/Dialog.jsx
+++ admin/src/features/common/Dialog.jsx
@@ -3,21 +3,21 @@ import { createPortal } from 'react-dom'
 
 const Dialog = (props) =>
   createPortal(
-    // TODO: convert css to tailwind
-    <div {...props.attr.container} className="dialog-container">
-      <div {...props.attr.overlay} className="dialog-overlay"/>
-      <div {...props.attr.dialog} className="dialog-content bg-white border-black border border-solid border-spacing-1 m-auto z-20 relative py-3 px-5 w-min-300px">
-        <div className="dialog-header">
-          <p {...props.attr.title} className="dialog-title">
+    
+    <div {...props.attr.container} className="dialog-container z-10 flex fixed inset-0">
+      <div {...props.attr.overlay} className="backdrop-blur-sm opacity-75 bg-white fixed inset-0"/>
+      <div {...props.attr.dialog} className="bg-white border-sky-300 border-2 border-solid rounded-xl shadow-lg m-auto z-20 relative py-3 px-5 w-min-300px">
+        <div className="flex justify-between items-center">
+          <p {...props.attr.title} className="font-bold text-2xl text-sky-600">
             {props.title}
           </p>
-          <button {...props.attr.closeButton} className="dialog-close">
+          <button {...props.attr.closeButton} className="cursor-pointer p-2">
             {/* <FontAwesomeIcon icon="fa-solid fa-xmark" /> */}
             ❌
           </button>
         </div>
 
-        <div className="dialog-children">{props.children}</div>
+        <div className="pt-7">{props.children}</div>
       </div>
     </div>,
     document.body,
