9a654fecabdd09911099e5ca9934b29be3832166
21120508 <ladykillal1232@gmail.com>
2023-12-02 22:19:52 +0700

    Add form for booking a clinic

---
+++ user/src/pages/Booking.jsx
@@ -0,0 +1,22 @@
+import BookingForm from "../features/bookClinic/BookingForm";
+import Footer from "../ui/Footer";
+import Header from "../ui/Header";
+
+function Booking() {
+  return (
+    <div className="container-2xl bg-neutral-200 ">
+      <div className="flex h-screen flex-col items-center justify-between">
+        <Header />
+        <div className="font-['Lexend Deca'] m-4 self-start text-4xl font-normal text-black">
+          Đặt lịch hẹn
+        </div>
+        <div className="flex w-screen grow items-center justify-center gap-52">
+          <BookingForm />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Booking;
