63fac0586bfc3f318140fcb4e075103b14525cab
cukhoaimon <phucmapcaumieu@gmail.com>
2024-01-18 10:08:38 +0700

    feat: fetch dentist

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,20 +1,21 @@
-const sql = require('mssql');
+const sql = require("mssql");
 
 const connectionOptions = {
-    server: process.env.SQL_SERVER,
-    database: process.env.SQL_DATABASE,
-    user: process.env.SQL_UID,
-    password: process.env.SQL_PWD,
-    options: {
-        encrypt: false,
-        enableArithAbort: false,
-    },
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000,
-    },
-}
+  server: process.env.SQL_SERVER,
+  database: process.env.SQL_DATABASE,
+  user: process.env.SQL_UID,
+  password: process.env.SQL_PWD,
+  port: 1443,
+  options: {
+    encrypt: false,
+    enableArithAbort: false,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+};
 
 const pool = new sql.ConnectionPool(connectionOptions);
 
